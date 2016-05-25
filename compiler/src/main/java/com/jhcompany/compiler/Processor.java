5bfaddf4cf3e1ad7c459a928fcf9925a8c73ba13
Jason Kim <jason@vcnc.co.kr>
2016-05-25 18:26:02 +0900

    Factory Annotation 추가, 기본적인 Processor function Override

--- compiler/src/main/java/com/jhcompany/compiler/Processor.java
+++ compiler/src/main/java/com/jhcompany/compiler/Processor.java
@@ -1,34 +1,61 @@
 package com.jhcompany.compiler;
 
 import com.google.auto.service.AutoService;
+import com.jhcompany.annotations.Factory;
+import com.jhcompany.compiler.model.FactoryGroupedClasses;
 
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Filer;
+import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.util.Elements;
+import javax.lang.model.util.Types;
 
 @AutoService(javax.annotation.processing.Processor.class)
 public class Processor extends AbstractProcessor {
+    private Types typeUtils;
+    private Elements elementUtils;
+    private Filer filer;
+    private Messager messager;
+    private Map<String, FactoryGroupedClasses> factoryClasses = new LinkedHashMap<>();
+
     @Override
     public synchronized void init(ProcessingEnvironment processingEnv) {
         super.init(processingEnv);
+        typeUtils = processingEnv.getTypeUtils();
+        elementUtils = processingEnv.getElementUtils();
+        filer = processingEnv.getFiler();
+        messager = processingEnv.getMessager();
     }
 
     @Override
     public Set<String> getSupportedAnnotationTypes() {
-        return super.getSupportedAnnotationTypes();
+        Set<String> annotations = new LinkedHashSet<>();
+        annotations.add(Factory.class.getCanonicalName());
+        return annotations;
     }
 
     @Override
     public SourceVersion getSupportedSourceVersion() {
-        return super.getSupportedSourceVersion();
+        return SourceVersion.latestSupported();
     }
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        // Iterate over all @Factory annotated elements
+        for (Element annotatedElement : roundEnv.getElementsAnnotatedWith(Factory.class)) {
+        }
+        // TODO:
+
         return false;
     }
 }
---
+++ compiler/src/main/java/com/jhcompany/compiler/model/FactoryGroupedClasses.java
@@ -0,0 +1,4 @@
+package com.jhcompany.compiler.model;
+
+public class FactoryGroupedClasses {
+}
