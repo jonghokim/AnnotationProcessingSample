49de9640e41774f5256a168b2c08521806571d9f
jonghokim <kjhichy@gmail.com>
2016-05-26 12:00:28 +0900

    Processor 구현완료

--- compiler/src/main/java/com/jhcompany/compiler/Processor.java
+++ compiler/src/main/java/com/jhcompany/compiler/Processor.java
@@ -2,8 +2,11 @@ package com.jhcompany.compiler;
 
 import com.google.auto.service.AutoService;
 import com.jhcompany.annotations.Factory;
+import com.jhcompany.compiler.exception.ProcessingException;
+import com.jhcompany.compiler.model.FactoryAnnotatedClass;
 import com.jhcompany.compiler.model.FactoryGroupedClasses;
 
+import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.Map;
@@ -16,12 +19,15 @@ import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.TypeKind;
+import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.Elements;
 import javax.lang.model.util.Types;
 import javax.tools.Diagnostic;
-
-import static javax.tools.Diagnostic.Kind.*;
 
 @AutoService(javax.annotation.processing.Processor.class)
 public class Processor extends AbstractProcessor {
@@ -56,10 +62,132 @@ public class Processor extends AbstractProcessor {
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         // Iterate over all @Factory annotated elements
         for (Element annotatedElement : roundEnv.getElementsAnnotatedWith(Factory.class)) {
-            messager.printMessage(NOTE, "round");
-        }
-        // TODO:
+            messager.printMessage(Diagnostic.Kind.NOTE, "start");
+            // Check if a class has been annotated with @Factory
+            if (annotatedElement.getKind() != ElementKind.CLASS) {
+                error(annotatedElement, "Only classes can be annotated with @%s", Factory.class.getSimpleName());
+                return true;
+            }
+            // We can cast it, because we know that it of ElementKind.CLASS
+            TypeElement typeElement = (TypeElement) annotatedElement;
+            try {
+                // throws IllegalArgumentException
+                messager.printMessage(Diagnostic.Kind.NOTE, "FactoryAnnotatedClass");
+                FactoryAnnotatedClass annotatedClass = new FactoryAnnotatedClass(typeElement);
+                if (!isValidClass(annotatedClass)) {
+                    // Error message printed, exit processing
+                    return true;
+                }
+                // Everything is fine, so try to add
+                FactoryGroupedClasses factoryClass =
+                        factoryClasses.get(annotatedClass.getQualifiedFactoryGroupName());
+                if (factoryClass == null) {
+                    String qualifiedGroupName = annotatedClass.getQualifiedFactoryGroupName();
+                    factoryClass = new FactoryGroupedClasses(qualifiedGroupName);
+                    factoryClasses.put(qualifiedGroupName, factoryClass);
+                }
 
+                // Throws ProcessingException if id is conflicting with
+                // another @Factory annotated class with the same id
+                factoryClass.add(annotatedClass);
+            } catch (IllegalArgumentException e) {
+                // @Factory.id() is empty
+                error(typeElement, e.getMessage());
+                return true;
+            } catch (ProcessingException e) {
+                error(typeElement, e.getMessage());
+                return true;
+            }
+        }
+
+        // generate code
+        try {
+            for (FactoryGroupedClasses factoryClass : factoryClasses.values()) {
+                messager.printMessage(Diagnostic.Kind.NOTE, "generate");
+                factoryClass.generateCode(elementUtils, filer);
+            }
+            factoryClasses.clear();
+        } catch (IOException e) {
+            error(null, e.getMessage());
+        }
+
+        return true;
+    }
+
+    private void error(Element e, String msg, Object... args) {
+        messager.printMessage(
+                Diagnostic.Kind.ERROR,
+                String.format(msg, args),
+                e);
+    }
+
+    private boolean isValidClass(FactoryAnnotatedClass item) {
+        // Cast to TypeElement, has more type specific methods
+        TypeElement classElement = item.getTypeElement();
+
+        if (!classElement.getModifiers().contains(Modifier.PUBLIC)) {
+            error(classElement, "The class %s is not public.",
+                    classElement.getQualifiedName().toString());
+            return false;
+        }
+
+        // Check if it's an abstract class
+        if (classElement.getModifiers().contains(Modifier.ABSTRACT)) {
+            error(classElement, "The class %s is abstract. You can't annotate abstract classes with @%",
+                    classElement.getQualifiedName().toString(), Factory.class.getSimpleName());
+            return false;
+        }
+
+        // Check inheritance: Class must be child class as specified in @Factory.type();
+        TypeElement superClassElement =
+                elementUtils.getTypeElement(item.getQualifiedFactoryGroupName());
+        if (superClassElement.getKind() == ElementKind.INTERFACE) {
+            // Check interface implemented
+            if (!classElement.getInterfaces().contains(superClassElement.asType())) {
+                error(classElement, "The class %s annotated with @%s must implement the interface %s",
+                        classElement.getQualifiedName().toString(), Factory.class.getSimpleName(),
+                        item.getQualifiedFactoryGroupName());
+                return false;
+            }
+        } else {
+            // Check subclassing
+            TypeElement currentClass = classElement;
+            while (true) {
+                TypeMirror superClassType = currentClass.getSuperclass();
+
+                if (superClassType.getKind() == TypeKind.NONE) {
+                    // Basis class (java.lang.Object) reached, so exit
+                    error(classElement, "The class %s annotated with @%s must inherit from %s",
+                            classElement.getQualifiedName().toString(), Factory.class.getSimpleName(),
+                            item.getQualifiedFactoryGroupName());
+                    return false;
+                }
+
+                if (superClassType.toString().equals(item.getQualifiedFactoryGroupName())) {
+                    // Required super class found
+                    break;
+                }
+
+                // Moving up in inheritance tree
+                currentClass = (TypeElement) typeUtils.asElement(superClassType);
+            }
+        }
+
+        // Check if an empty public constructor is given
+        for (Element enclosed : classElement.getEnclosedElements()) {
+            if (enclosed.getKind() == ElementKind.CONSTRUCTOR) {
+                ExecutableElement constructorElement = (ExecutableElement) enclosed;
+                if (constructorElement.getParameters().size() == 0 && constructorElement.getModifiers()
+                        .contains(Modifier.PUBLIC)) {
+                    // Found an empty constructor
+                    return true;
+                }
+            }
+        }
+
+        // No empty constructor found
+        error(classElement, "The class %s must provide an public empty default constructor",
+                classElement.getQualifiedName().toString());
         return false;
     }
 }
---
+++ library/src/main/java/com/jhcompany/library/FactoryBuilder.java
@@ -0,0 +1,5 @@
+package com.jhcompany.library;
+
+public class FactoryBuilder {
+
+}
